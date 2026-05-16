cdd1e615931c417f8093e7302fa20e3be655b912
Berdina93 <chalisseyokani@gmail.com>
2026-05-16 20:37:15 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,9 +5,9 @@
 // ============================================================
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'projet_qcm');
+define('DB_NAME', 'project_qcm');
 define('DB_USER', 'root');       // ← à adapter selon votre config XAMPP/WAMP
-define('DB_PASS', '');           // ← idem
+define('DB_PASS', 'root');          // ← idem
 
 try {
     $pdo = new PDO(
